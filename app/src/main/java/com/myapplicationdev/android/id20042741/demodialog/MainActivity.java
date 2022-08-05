bbe6678008b171a36220eee2437b82ff6c0967d0
Yeow Heng <20042741@myrp.edu.sg>
2022-08-05 09:22:47 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/id20042741/demodialog/MainActivity.java
@@ -0,0 +1,14 @@
+package com.myapplicationdev.android.id20042741.demodialog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
